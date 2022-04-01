353bfb5fae672ba3f8db5eb6f150d14df9607319
feezee <brian.deese30@gmail.com>
2022-03-31 20:54:32 -0700

    app.js and appspec.yml update

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express ();
-const port =4000;
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Express Demo App</h1> <h4>With Node JS</h4> <p>Being updated via Elastic Beanstalk in AWS</p>');
-})
+});
 
-app.get('/', (req, res) => {
-    res.send("Welcome to the home page of this Application");
-})   
-
-app.listen(4000, ()=> {
+const port = process.env.port || 3000;
+app.listen(port, ()=> {
     console.log('Demo app is up and listening to port: ${port}');
 });
