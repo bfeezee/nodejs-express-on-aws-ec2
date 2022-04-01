fe5e3fc310b6aa70dbbfc0d5e6d56a5e4ccaa4f0
bfeezee <brian.deese30@gmail.com>
2022-04-01 14:24:38 -0700

    version 6 update (#5)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('<h1>This is Version 5 of this Express Demo Application</h1> <h4>With Node JS</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 4000;
 app.listen(port, ()=> {
     console.log('Demo app is up and listening to port: ${port}');
 });
