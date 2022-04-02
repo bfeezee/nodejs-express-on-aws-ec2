001fc2a33a7515733fdfad3f08598970d1ee269a
feezee <brian.deese30@gmail.com>
2022-04-02 09:49:37 -0700

    sixth update made 4-2-22

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ();
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>This is Version 5 of this Express Demo Application</h1> <h4>With Node JS</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
+    res.send('<h1>This is Version 6 of this Express Demo Application--Update made on Sat 4-2-2022</h1> <h4>With Node JS</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
 });
 
 const port = process.env.port || 4000;
