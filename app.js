ba621bc9627ec5978b26b4d326ba31bbd0bbdf16
feezee <brian.deese30@gmail.com>
2022-04-02 12:53:25 -0700

    deleted tacos image

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express ();
-var img = document.createElement("img");
-img.src = "/Users/brian-deese/nodejs-express-on-aws-ec2/birriaTacos.jpeg";
 
 
 app.get('/', (req, res) => {
     res.send('<div id="x"></div><h1>This is Version 7 of this Express Demo Application--Update made on Sat 4-2-2022 by Brian Deese</h1> <h4>With Node JS</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
 ii});
 
-var block = document.getElementById("x");
-block.appendChild(img);
 
 const port = process.env.port || 4000;
 app.listen(port, ()=> {
