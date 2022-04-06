0a6b5e9b2d51191774d7068966ee1d4c5620f167
bfeezee <brian.deese30@gmail.com>
2022-04-06 09:37:11 -0700

    update 11

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express ();
 
 
 app.get('/', (req, res) => {
-    res.send('</div>
-<h1>This is Version 9 of this Express Demo Application--Update made on Sat 4-2-2022 by Brian Deese</h1>
- <h4>With Node JS Taco image didnt upload</h4><h3>Update on wed 4-6-22</h3> 
-<p>Currently linked to  Elastic Beanstalk in AWS</p>');
-
+    res.send('</div><h1>This is Version 9 of this Express Demo Application--Update made on Sat 4-2-2022 by Brian Deese</h1> <h4>With Node JS Taco image didnt upload</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
 });
 
 const port = process.env.port || 4000;
