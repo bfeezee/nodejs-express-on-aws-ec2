1325b6c3b26c033ce8c5bb8dd04ee6cee16a625c
bfeezee <brian.deese30@gmail.com>
2022-04-05 23:34:23 -0700

    app update 10, added deeseautosales.com domain (#10)

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express ();
 
 app.get('/', (req, res) => {
     res.send('</div><h1>This is Version 9 of this Express Demo Application--Update made on Sat 4-2-2022 by Brian Deese</h1> <h4>With Node JS Taco image didnt upload</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
-ii});
+    ii});
+<div>
+Making new update for sync with Brandon on 4/6/2022
+</div>
 
-
+alert
 const port = process.env.port || 4000;
 app.listen(port, ()=> {
     console.log('Demo app is up and listening to port: ${port}');
