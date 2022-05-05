547d918712d1ad7a097ccf23a03c9febcbd3d0de
bfeezee <brian.deese30@gmail.com>
2022-05-05 00:14:46 -0700

    Revert "Interview update (#16)" (#20)
    This reverts commit b07166fcd13e1c421b5dbefb75a22861b8cd8bab.

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const app = express ();
 
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-    res.send('</div><h1>This is Version 11 of this Express Demo Application--Update made by Brian on Wednesday 5-6-2022 by Brian Deese</h1><h2>added this line prior to interview with Ryan on Thursday morning</h2> <h4>This was built as a node.js application</h4> <p>The code is being house in GitHub under ther "New" repository, auto-fetched by the "App5 pipeline created and delivered via  Elastic Beanstalk in AWS</p>');
-=======
-    res.send('</div><h1>This is Version 11 of this Express Demo Application--Update made on Sat 4-6-2022 by Brian Deese</h1><h2>added this line prior to sync with Brandon</h2> <h4>With Node JS Taco image didnt upload</h4> <p>Currently linked to  Elastic Beanstalk in AWS</p>');
->>>>>>> 16abc994e0b006a0251cc9a05a72030c866ca38d
+    res.send('</div><h1>This is Version 11 of this Express Demo Application--Update made on Wednesday 4-6-2022 by Brian Deese</h1><h2>added this line prior to sync with Brandon</h2> <h4>This was built as a node.js application</h4> <p>The code is being house in GitHub under ther "New" repository, auto-fetched by the "App5 pipeline created and delivered via  Elastic Beanstalk in AWS</p>');
 });
 
 const port = process.env.port || 4000;
